cfa17d3f2a7fb4efb65dec8cd345fc0f98b38852
rik <hrvanderlingen@gmail.com>
2019-11-02 18:59:30 +0100

    environment update

--- tree-project/src/environments/environment.prod.ts
+++ tree-project/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   jsonplaceholder_url: 'https://jsonplaceholder.typicode.com',
-  tree_rest_url: 'http://orinoco.vander-lingen.nl'
+  tree_rest_url: 'http://orinoco.vander-lingen.nl',
+  rsc_url: 'https://api.rsc.org/compounds/v1/lookups'
 };
